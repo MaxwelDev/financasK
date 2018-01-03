6325b7e857fe25d26eb22ae01f799d3aadd33696
MaxwelDev <maxwel.silva2@fatec.sp.gov.br>
2018-01-03 17:57:56 -0200

    create first screen

---
+++ app/src/main/java/br/com/financas/financask/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,14 @@
+package br.com.financas.financask.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import br.com.financas.financask.R
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+
+}
