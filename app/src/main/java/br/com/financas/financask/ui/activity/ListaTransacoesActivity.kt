e1c0f315f45e7c20bec22728362da6d4fa49331f
MaxwelDev <maxwel.silva2@fatec.sp.gov.br>
2018-01-12 10:23:17 -0200

    list creation of methods

--- app/src/main/java/br/com/financas/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/financas/financask/ui/activity/ListaTransacoesActivity.kt
@@ -2,13 +2,21 @@ package br.com.financas.financask.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
 import br.com.financas.financask.R
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
-    }
+        val transacoes = listOf("Comida - R$ 20,50", "Economia - R$ 100,00")
+//        transacoes = listOf("Carro - R$ 10000,00", "Computador - R$ 2000,00")
 
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, transacoes)
+
+        lista_transacoes_listview.setAdapter(arrayAdapter)
+
+    }
 }
