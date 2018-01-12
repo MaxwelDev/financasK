60e713ce9f783cc65657e34009d805ca3640c3ed
MaxwelDev <maxwel.silva2@fatec.sp.gov.br>
2018-01-12 19:52:57 -0200

    modification in screen

--- app/src/main/java/br/com/financas/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/financas/financask/ui/activity/ListaTransacoesActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
 import br.com.financas.financask.R
+import br.com.financas.financask.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
@@ -12,11 +13,11 @@ class ListaTransacoesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
         val transacoes = listOf("Comida - R$ 20,50", "Economia - R$ 100,00")
-//        transacoes = listOf("Carro - R$ 10000,00", "Computador - R$ 2000,00")
 
         val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, transacoes)
 
-        lista_transacoes_listview.setAdapter(arrayAdapter)
+        lista_transacoes_listview.setAdapter(
+                ListaTransacoesAdapter(transacoes, this))
 
     }
 }
